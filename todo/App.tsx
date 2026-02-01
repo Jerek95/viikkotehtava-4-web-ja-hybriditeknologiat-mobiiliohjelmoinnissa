f37c35141fbda35ec9f44007deb9c928f1cf8b7a
Jere Koivuranta <jere.koivuranta@gmail.com>
2026-02-01 15:17:36 +0200

    viikkotehtava 4 tehty

--- todo/App.tsx
+++ todo/App.tsx
@@ -1,72 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, FlatList, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Row from './components/Row';
 import { Task } from './types/Task';
 import * as SQLite from 'expo-sqlite';
 
-const [inputText, setInputText] = useState<string>('')
-const [tasks, setTasks] = useState<Task[]>([])
-const [db, setDb] = useState<SQLite.SQLiteDatabase | null>(null);
 
-useEffect(() => {
-    const initDB = async () => {
-      const database = await SQLite.openDatabaseAsync('tasks.db');
-      setDb(database);
-
-      await database.execAsync(`
-        CREATE TABLE IF NOT EXISTS tasks (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          text TEXT NOT NULL,
-          done INTEGER DEFAULT 0
-        );
-      `);
-
-      loadTasks(database);
-    };
-
-    initDB();
-  }, []);
-
-const loadTasks = async (db: SQLite.SQLiteDatabase) => {
-  const result = await db.getAllAsync<Task>('SELECT * FROM tasks ORDER BY id DESC');
-    setTasks(result);
-}
-
-const addTask = async () => {
-  if (!inputText.trim() || !db) return;
-
-    await db.runAsync('INSERT INTO tasks (text) VALUES (?)', inputText);
-    setInputText('');
-    loadTasks(db);
-}
-
-const toggleTask = async (id: number, done: number) => {
-  if (!db) return;
-  await db.runAsync('UPDATE tasks SET done = ? WHERE id = ?', done ? 0 : 1, id);
-  loadTasks(db);
-}
-
-const deleteTask = async (id: number) => {
-  if (!db) return;
-  await db.runAsync('DELETE FROM tasks WHERE id = ?', id);
-  loadTasks(db);
-}
 
 export default function App() {
+  const [inputText, setInputText] = useState<string>('')
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [db, setDb] = useState<SQLite.SQLiteDatabase | null>(null);
+
+  useEffect(() => {
+      const initDB = async () => {
+        const database = await SQLite.openDatabaseAsync('tasks.db');
+        setDb(database);
+
+        await database.execAsync(`
+          CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            text TEXT NOT NULL,
+            done INTEGER DEFAULT 0
+          );
+        `);
+
+        loadTasks(database);
+      };
+
+      initDB();
+    }, []);
+
+  const loadTasks = async (db: SQLite.SQLiteDatabase) => {
+    const result = await db.getAllAsync<Task>('SELECT * FROM tasks ORDER BY id DESC');
+      setTasks(result);
+  }
+
+  const addTask = async () => {
+    if (!inputText.trim() || !db) return;
+
+      await db.runAsync('INSERT INTO tasks (text) VALUES (?)', inputText);
+      setInputText('');
+      loadTasks(db);
+  }
+
+  const toggleTask = async (id: number, done: number) => {
+    if (!db) return;
+    await db.runAsync('UPDATE tasks SET done = ? WHERE id = ?', done ? 0 : 1, id);
+    loadTasks(db);
+  }
+
+  const deleteTask = async (id: number) => {
+    if (!db) return;
+    await db.runAsync('DELETE FROM tasks WHERE id = ?', id);
+    loadTasks(db);
+  }
+  
+  
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Todo List</Text>
-      <TextInput
+      <Text style={styles.header}>Todo List</Text>
+      <View style={styles.input}>
+        <TextInput
           placeholder="Enter task..."
           value={inputText}
           onChangeText={setInputText}
           onSubmitEditing={addTask}
         />
-        <TouchableOpacity onPress={addTask}>
+        <TouchableOpacity onPress={addTask} style={styles.addButton}>
           <Text>Add</Text>
         </TouchableOpacity>
+      </View>
+      
         <FlatList
         data={tasks}
         keyExtractor={(item) => item.id.toString()}
@@ -82,8 +88,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ffffff49',
+    justifyContent: 'space-between',
   },
+  header: {
+    fontSize: 24,
+    margin: 16,
+    textAlign: 'center'
+  },
+  input:{
+    alignItems: 'center'
+  },
+  addButton: {
+    backgroundColor: '#17bb11a5',
+    padding: 8
+  }
 });
--- todo/components/Row.tsx
+++ todo/components/Row.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
 import { Task } from '../types/Task'
 
@@ -10,19 +10,36 @@ interface TaskItemProps {
 
 export default function Row({ item, toggleTask, deleteTask } : TaskItemProps) {
   return (
-    <View>
+    <View style={styles.row}>
       <TouchableOpacity
         onPress={() => toggleTask(item.id, item.done)}
       >
-        <Text>
+        <Text style={[item.done ? styles.itemDone : null]}>
           {item.text}
         </Text>
       </TouchableOpacity>
       <TouchableOpacity
         onPress={() => deleteTask(item.id)}
+        style={styles.deleteButton}
       >
         <Text>Delete</Text>
       </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    margin: 16,
+    backgroundColor: '#ffffff37',
+    justifyContent: 'space-between',
+  },
+  itemDone:{
+    textDecorationLine: 'line-through'
+  },
+  deleteButton:{
+    backgroundColor: 'red',
+    padding: 4
+  }
+});
